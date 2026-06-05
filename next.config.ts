61f9201a3e6ad353ee8e2d3edfc92a8a6e00e055
Pangoth Hemanth Nayak <hemanthnayakpangoth@gmail.com>
2026-06-05 15:40:50 +0530

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,24 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let repo = undefined;
+
+if (isGithubActions) {
+  const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1];
+  if (repoName) {
+    repo = `/${repoName}`;
+  }
+}
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: "export",
+  basePath: repo,
+  assetPrefix: repo,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
